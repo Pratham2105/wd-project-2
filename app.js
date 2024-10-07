cd068fa89561e5d001c948ab2f7d21e22ddfc1d7
Pratham2105 <146621335+Pratham2105@users.noreply.github.com>
2024-10-07 23:01:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-  
-    alert(`Thank you for contacting us, ${name}! We'll get back to you soon at ${email}.`);
-  });
+
   
